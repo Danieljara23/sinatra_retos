2d20ff055e1676944b7a57d4dfa61be41df66461
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-10-20 18:32:06 -0500

    Reto 9

--- solution.rb
+++ solution.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 
 get '/' do
   if request.env["HTTP_PERMISO"] == "soy-un-token-secreto"
-    puts "Si lo logramos"
+    puts "Si lo logramos!"
   else
     puts "Sin Permiso"
   end
