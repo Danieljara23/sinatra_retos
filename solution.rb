405c832f391d6dee5ebac0002bac89e3e4d508e3
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-10-20 18:28:42 -0500

    Reto 9

--- solution.rb
+++ solution.rb
@@ -1,13 +1,11 @@
 require 'sinatra'
 
 
+
 get '/' do
-  headers = Hash[*env.select {|k,v| k.start_with? 'HTTP_'}
-    .collect {|k,v| [k.sub(/^HTTP_/, ''), v]}
-    .collect {|k,v| [k.split('_').collect(&:capitalize).join('-'), v]}
-    .sort
-    .flatten]
-  #puts headers
-  @header = headers
-  erb :index
+  if request.env["HTTP_PERMISO"] == "soy-un-token-secreto"
+    puts "Si lo logramos"
+  else
+    puts "Sin Permiso"
+  end
 end
